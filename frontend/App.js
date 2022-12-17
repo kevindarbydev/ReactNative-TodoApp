49914f21f0eedaf740035e034f47603068338352
SeenaSK <104446906+SeenaSK@users.noreply.github.com>
2022-12-17 12:39:24 -0500

    File Hierarchy Reorganization

--- frontend/App.js
+++ frontend/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { AuthProvider } from "./hooks/useAuth";
-import StackNavigator from "./StackNavigator";
+import { AuthProvider } from "./src/hooks/useAuth";
+import StackNavigator from "./src/navigation/StackNavigator";
 
 export default function App() {
-
   return (
     <NavigationContainer>
       {/* HOC - Higher Order Component */}
       <AuthProvider>
         {/* Passes down the cool auth stuff to children... */}
         <StackNavigator />
-
       </AuthProvider>
     </NavigationContainer>
   );
-};
+}
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,17 @@
 import React, { useLayoutEffect, useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import client from "../sanity";
-import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import Task from "../components/Task.js";
+import client from "../../sanity";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Task from "../components/Task";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -16,13 +25,13 @@ const HomeScreen = () => {
     Keyboard.dismiss();
     setTaskItems([...taskItems, task]);
     setTask(null);
-  }
+  };
 
   const completeTask = (index) => {
     let itemsCopy = [...taskItems];
     itemsCopy.splice(index, 1);
     setTaskItems(itemsCopy);
-  }
+  };
 
   // Removes default header
   useLayoutEffect(() => {
@@ -45,22 +54,27 @@ const HomeScreen = () => {
 
         <View style={styles.items}>
           {/* This is where the tasks will go */}
-          {
-            taskItems.map((item, index) => {
-              return (
-                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
-                  <Task text={item} />
-                </TouchableOpacity>
-              )
-            })
-          }
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            );
+          })}
         </View>
-
       </View>
 
       {/* Write a Task */}
-      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.writeTask}>
-        <TextInput style={styles.input} placeholder={'Write a Task'} onChangeText={text => setTask(text)} value={task} />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTask}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a Task"}
+          onChangeText={(text) => setTask(text)}
+          value={task}
+        />
         <TouchableOpacity onPress={handleAddTask}>
           <View style={styles.addWrapper}>
             <Text style={styles.addText}>+</Text>
@@ -69,13 +83,12 @@ const HomeScreen = () => {
       </KeyboardAvoidingView>
     </View>
   );
-
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8EAED',
+    backgroundColor: "#E8EAED",
   },
   taskWrapper: {
     paddingTop: 80,
@@ -83,41 +96,39 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   items: {
     marginTop: 30,
   },
   writeTask: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 60,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
   },
   input: {
     paddingVertical: 15,
     paddingHorizontal: 15,
     width: 250,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 60,
-    borderColor: '#C0C0C0',
+    borderColor: "#C0C0C0",
     borderWidth: 1,
   },
   addWrapper: {
     width: 60,
     height: 60,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#C0C0C0',
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
     borderWidth: 1,
   },
-  addText: {
-
-  },
+  addText: {},
 });
 
 export default HomeScreen;
