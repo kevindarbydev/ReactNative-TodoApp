746e44988a9a216583327f1927814e2205ae12d6
MattReda11 <103293959+MattReda11@users.noreply.github.com>
2022-12-17 12:20:36 -0500

    added To-do list tut app

--- frontend/App.js
+++ frontend/App.js
@@ -1,16 +1,18 @@
-import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./hooks/useAuth";
 import StackNavigator from "./StackNavigator";
 
 export default function App() {
+
   return (
     <NavigationContainer>
       {/* HOC - Higher Order Component */}
       <AuthProvider>
         {/* Passes down the cool auth stuff to children... */}
         <StackNavigator />
+
       </AuthProvider>
     </NavigationContainer>
   );
-}
+};
--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,11 +1,28 @@
-import { View, Text, SafeAreaView, Image, Button } from "react-native";
 import React, { useLayoutEffect, useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import client from "../sanity";
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from "../components/Task.js";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
   const [users, setUsers] = useState([]);
+
+  // From Tut:
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task]);
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
 
   // Removes default header
   useLayoutEffect(() => {
@@ -21,28 +38,86 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <SafeAreaView>
-      {/* Header */}
-      <View className="flex-row pb-3 items-center mx-4 space-x-2">
-        <Image
-          source={require("../assets/checkmark.png")}
-          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-        />
-        <View>
-          <Text>My To-Do List!</Text>
-          <Button
-            title="Go to Profile Page"
-            onPress={() => navigation.navigate("MyProfile")}
-          />
+    <View style={styles.container}>
+      {/* Today's task */}
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go */}
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item} />
+                </TouchableOpacity>
+              )
+            })
+          }
         </View>
+
       </View>
-      <View>
-        {users.map((user) => (
-          <Text key={user._id}>{user.email}</Text>
-        ))}
-      </View>
-    </SafeAreaView>
+
+      {/* Write a Task */}
+      <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.writeTask}>
+        <TextInput style={styles.input} placeholder={'Write a Task'} onChangeText={text => setTask(text)} value={task} />
+        <TouchableOpacity onPress={handleAddTask}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View>
   );
+
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+  },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
+  },
+  writeTask: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    width: 250,
+    backgroundColor: 'white',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: 'white',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {
+
+  },
+});
 
 export default HomeScreen;
