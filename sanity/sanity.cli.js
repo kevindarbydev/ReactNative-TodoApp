9948fab04b1753be2eaf1b2e4c2d90700ec1879e
SeenaSK <104446906+SeenaSK@users.noreply.github.com>
2022-12-17 11:05:00 -0500

    Deleted broken backend
    Lost Authentication to deploy somehow

--- sanity/sanity.cli.js
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-  api: {
-    projectId: 'ja4gf4ug',
-    dataset: 'production'
-  }
-})
--- sanity/schemas/index.js
+++
@@ -1,4 +0,0 @@
-import blockContent from './blockContent'
-import user from './user'
-
-export const schemaTypes = [user, blockContent]
