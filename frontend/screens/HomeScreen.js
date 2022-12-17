b58b58b2b504f71b94303f30a20b96d9d6b41ceb
SeenaSK <104446906+SeenaSK@users.noreply.github.com>
2022-12-17 11:25:34 -0500

    displaying data from backend on homescreen
    -Added @sanity/client dependency
    npm install @sanity/client

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import { View, Text, SafeAreaView, Image, Button } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import client from "../sanity";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [users, setUsers] = useState([]);
 
   // Removes default header
   useLayoutEffect(() => {
@@ -12,15 +14,32 @@ const HomeScreen = () => {
     });
   });
 
+  useEffect(() => {
+    client.fetch(`*[_type == "user"]`).then((data) => {
+      setUsers(data);
+    });
+  }, []);
+
   return (
     <SafeAreaView>
       {/* Header */}
       <View className="flex-row pb-3 items-center mx-4 space-x-2">
-        <Image source={require("../assets/checkmark.png")} className="h-7 w-7 bg-gray-300 p-4 rounded-full" />
+        <Image
+          source={require("../assets/checkmark.png")}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
         <View>
           <Text>My To-Do List!</Text>
-          <Button title="Go to Profile Page" onPress={() => navigation.navigate("MyProfile")} />
+          <Button
+            title="Go to Profile Page"
+            onPress={() => navigation.navigate("MyProfile")}
+          />
         </View>
+      </View>
+      <View>
+        {users.map((user) => (
+          <Text key={user._id}>{user.email}</Text>
+        ))}
       </View>
     </SafeAreaView>
   );
