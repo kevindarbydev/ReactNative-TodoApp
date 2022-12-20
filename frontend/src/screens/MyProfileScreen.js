455457ce9ef2870b2ba86b7c1f93ee075d1743f2
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2022-12-20 11:10:46 -0500

    bit of formatting on profile screen

--- frontend/src/screens/MyProfileScreen.js
+++ frontend/src/screens/MyProfileScreen.js
@@ -1,14 +1,21 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, FlatList } from "react-native";
 import React from "react";
 
 const MyProfileScreen = () => {
   return (
-    
-    <ScrollView className="flex-1 bg-gray-200">
-      <Text className="font-semibold text-base mt-5">
-        Here you can see your completed tasks!
-      </Text>      
-    </ScrollView>
+    <View
+      className="flex-1 bg-gray-200 items-center"
+      // contentContainerStyle={{ flexGrow: 1, alignItems: "center" }}
+    >
+      <Text className="font-bold text-2xl mt-5">Completed Tasks</Text>
+      <FlatList // hardcoded for now
+        data={[{ key: "Walk the cat" }]}
+        renderItem={({ item }) => (
+          <Text className="text-base mt-2">{item.key} ✔ </Text>
+        )}
+      />
+     
+    </View>
   );
 };
 
