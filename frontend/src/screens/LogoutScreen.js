1a7a3b251ffbf787bba0678653b14677b948b51f
SeenaSK <104446906+SeenaSK@users.noreply.github.com>
2022-12-21 09:21:37 -0500

    Set user Object to empty array on logout

--- frontend/src/screens/LogoutScreen.js
+++ frontend/src/screens/LogoutScreen.js
@@ -3,18 +3,24 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { AuthContext } from "../hooks/useAuth";
 
 export default function LogoutButton() {
-    const { setIsLoggedIn } = React.useContext(AuthContext);
+  const { setIsLoggedIn, setUser } = React.useContext(AuthContext);
 
-    const handleLogOut = () => {
-        setIsLoggedIn(false);
-    }
+  const handleLogOut = () => {
+    setIsLoggedIn(false);
+    setUser([]);
+  };
 
-    return (
-        <View className="flex-1 bg-gray-200">
-            <Text className="text-2xl font-bold text-center">Are you sure you want to Log Out? You will not be able to access your Tasks once logged out.</Text>
-            <TouchableOpacity onPress={handleLogOut} className="bg-blue-500 items-center w-full py-4 rounded-lg">
-                <Text className="text-white font-semibold text-base">Log Out</Text>
-            </TouchableOpacity>
-        </View>
-    )
+  return (
+    <View className="flex-1 bg-gray-200">
+      <Text className="text-2xl font-bold text-center">
+        Are you sure you want to Log Out? You will not be able to access your Tasks once logged out.
+      </Text>
+      <TouchableOpacity
+        onPress={handleLogOut}
+        className="bg-blue-500 items-center w-full py-4 rounded-lg"
+      >
+        <Text className="text-white font-semibold text-base">Log Out</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
